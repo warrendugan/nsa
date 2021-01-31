809cd4a3608a4b6810f5d469cc80123e3e75d9a9
Warren Dugan <warrendugan@gmail.com>
2021-01-30 21:41:09 -0800

    chore(root): clean up old project in jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/apps/www', '<rootDir>/apps/api', '<rootDir>/apps/coming-soon', '<rootDir>/apps/trash'],
+  projects: ['<rootDir>/apps/www', '<rootDir>/apps/api', '<rootDir>/apps/coming-soon'],
 }
